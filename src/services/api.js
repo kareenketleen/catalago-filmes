a6d060312cd69a393ca3163367084d719316ba70
Kareen Castro <kareen.ketleen@gmail.com>
2022-05-16 22:18:34 -0400

    update service

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import Axios from "axios";
 
 const api = Axios.create({
   baseURL: "https://api-catalogo-filmes.herokuapp.com",
+  //Mudar URL em DEV baseURL: "http://localhost:3333"
 });
 
 export default api;
